16ffe5f5970545322510a4b8579962d162bfae2d
Abdulaziz-rgb <shokirovabdulaziz19@gmail.com>
2022-10-28 15:21:41 +0500

    Iteachart Task 1 Solution

---
+++ task1_itechart/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task1_itechart
+{
+    public class Player
+    {
+        private int Rank;
+        private string Name;
+        private string Title;
+        private string Country;
+        private int Rating;
+        private int Games;
+        public int BirthYear;
+
+        public Player(string rowData)
+        {
+             string[] data = rowData.Split(';');
+            
+            // Parsing data into properties
+            this.Rank = Convert.ToInt16(data[0]);
+            this.Name = data[1];
+            this.Title = data[2];
+            this.Country = data[3];
+            this.Rating = Convert.ToInt16(data[4]);
+            this.Games = Convert.ToInt16(data[5]);
+            this.BirthYear = Convert.ToInt16(data[6]);
+        }
+        
+        // Overriding default Object.ToString() method into more readable format
+        public override string ToString()
+        {
+            string str = $"{Rank}: {Name} - {Title} - {Country} - {Rating} - {Games} - {BirthYear}";
+            return str;
+        }
+    }
+}
---
+++ task1_itechart/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using IronXL;
+using IronXL.Logging;
+using log4net;
+using log4net.Config;
+using static task1_itechart.Player;
+
+namespace task1_itechart
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Configuring log4net
+            BasicConfigurator.Configure();
+            
+            // Creating a Logger
+            ILog logger =  LogManager.GetLogger(typeof(Program));
+            
+            
+                
+            var path = Path.GetFullPath("Top100ChessPlayers.csv");    
+            // Reading the contents of the CSV file in the form of array strings
+            string[] csvLines = File.ReadAllLines(path);
+            
+            // List Collection to store players' data
+            var players = new List<Player>();
+
+            for (int i = 1; i < csvLines.Length; i++)
+            {
+                Player player = new Player(csvLines[i]);
+                players.Add(player);
+            }
+
+            // Getting first 10 players who are older 1980
+            var first_10_Players = players.OrderBy(player => player.BirthYear).Take(10);
+            
+            // Displaying 10 players
+            foreach (var player in first_10_Players)
+            {
+                // Displaying result in logger
+                logger.Info(player);
+            }
+        }
+    }
+}
